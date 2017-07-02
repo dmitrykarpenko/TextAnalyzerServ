cbc0d8193ae90feb85fea804effbb8897666fa12
dmitry karpenko <dmitry.karpenko@gmail.com>
2017-07-02 20:56:14 +0300

    Initial commit

---
+++ TextAnalyzerApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class UserText(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()  # blank=False
+    user = models.ForeignKey(User, related_name="texts")
+    year = models.IntegerField(null=True, blank = True)
+
+    def __str__(self):
+        return "Title: {}, text: {}".format(self.title, self.text)
