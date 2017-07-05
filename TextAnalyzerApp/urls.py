7194e95ff780f0c3e06d9648fd1d94de28e2780d
dmitry karpenko <dmitry.karpenko@gmail.com>
2017-07-06 01:26:56 +0300

    Angular home page demo

--- TextAnalyzerApp/urls.py
+++ TextAnalyzerApp/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from .api import UserApi, UserTextApi
 
 urlpatterns = [
+    # api
     url(r'^users$', UserApi.as_view()),
     url(r'^usertexts$', UserTextApi.as_view()),
+
+    # pages
+    url(r'^home', TemplateView.as_view(template_name="TextAnalyzer/home.html")),
 ]
